7dc6b7c26f8b6d252f592baad2db7b1c2270c366
jaimedantas <jaimejales@hotmail.com>
2022-03-25 16:27:32 -0400

    Chapter 4 - 1

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -4,8 +4,18 @@ import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    // 1011 is 1
+    // 10001000 is 0
+    long count = 0;
+    while (x != 0){
+      count += x & 1;
+      x >>= 1;
+    }
+    if (count%2==1){
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   public static void main(String[] args) {
